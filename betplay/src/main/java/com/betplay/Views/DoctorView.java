90a26da1757a973a1e09eed9cb6dda1a5e4999cf
carlosjhoan <carlosjhoanaguilar@gmail.com>
2024-06-25 16:21:43 -0500

    Sigue en creación la vista para doctores

--- betplay/src/main/java/com/betplay/Views/DoctorView.java
+++ betplay/src/main/java/com/betplay/Views/DoctorView.java
@@ -3,6 +3,10 @@ package com.betplay.Views;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import com.betplay.Controller;
+import com.betplay.model.entity.Doctor;
+import com.betplay.model.entity.Player;
+
 public class DoctorView {
 
     public DoctorView() {
@@ -27,6 +31,7 @@ public class DoctorView {
                 try {
                     System.out.println("\n¿Qué desea realizar?");
                     option = sc.nextInt();
+                    sc.nextLine();
                     validator = false;
                 }
 
@@ -40,16 +45,96 @@ public class DoctorView {
 
         switch (option) {
             case 1:
-                System.out.println("\n>>> Agregar Médico");
-                System.out.println("\nMédico agregado exitosamente.\nPresiona una tecla para volver al menú.");
+                Doctor myDoctor = new Doctor();
+                int Id;
+                String firstName;
+                String lastName;
+                int age;
+                String degree;
+                int expYear;
+                String keyTeamAdd;
+                String keyTeamEliminate;
+                
+                
+                System.out.println("\n> Código del equipo al cual va a pertenecer: ");
+                keyTeamAdd = sc.nextLine();
+                if (Controller.getController().controller.containsKey(keyTeamAdd) == true) {
+                    System.out.println("\n> Id del doctor: ");
+                    Id = sc.nextInt();
+                    sc.nextLine();
+                
+                    System.out.println("\n> Nombres del doctor: ");
+                    firstName = sc.nextLine();
+
+                    System.out.println("\n> Apellidos del doctor: ");
+                    lastName = sc.nextLine();
+
+                    System.out.println("\n> Edad del doctor: ");
+                    age = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.println("\n> Título del doctor: ");
+                    degree = sc.nextLine();
+                    
+                    
+                    System.out.println("\n> Años de experiencia del doctor: ");
+                    expYear = sc.nextInt();
+
+                    myDoctor.setId(Id);
+                    myDoctor.setFirstName(firstName);
+                    myDoctor.setLastName(lastName);
+                    myDoctor.setAge(age);
+                    myDoctor.setDegree(degree);
+                    myDoctor.setExpYear(expYear);
+                    Controller.getController().controller.get(keyTeamAdd).listDoctors.add(myDoctor);
+                    System.out.println("\n* * * * * * * * * * * * * * * * *");
+                    System.out.println("Doctor agregado exitosamente.\n* * * * * * * * * * * * * * * * *\n\n>>> Presiona una tecla para volver al menú.");
+                }
+
+                else {
+                    System.out.println("\n:: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: ::");
+                    System.out.println("::  No se ha creado ningún equipo con ese código.  ::\n:: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: ::");
+                    System.out.println("\n>>> Presiona una tecla para volver al menú.");
+                }    
+
                 sc.next();
                 DoctorMenu();
                 break;
         
             case 2:
-                System.out.println("\n>>> Eliminar médico");
-                System.out.println("\nMédico eliminado exitosamente.\nPresiona una tecla para volver al menú.");
-                sc.next();
+                boolean confirmation = false;
+                int indexEliminated = 0;
+                System.out.println("\n> Código del equipo del doctor: ");
+                keyTeamEliminate = sc.nextLine();
+                System.out.println("\n> Id del doctor: ");
+                Id = sc.nextInt();
+                sc.nextLine();
+                if (Controller.getController().controller.containsKey(keyTeamEliminate) == true) {
+                    for (Doctor doctor : Controller.getController().controller.get(keyTeamEliminate).listDoctors) {
+                        if (doctor.getId() == Id) {
+                            indexEliminated = Controller.getController().controller.get(keyTeamEliminate).listDoctors.indexOf(doctor);
+                            confirmation = true;
+                        }
+                    }
+
+                    if (confirmation == false) {
+                        System.out.println("\n:: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: ::");
+                        System.out.println("::  No se ha encontrado ningún doctor con ese Id.  ::\n:: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: ::");
+                        System.out.println("\n>>> Presiona una tecla para volver al menú.");
+                    }
+
+                    else{
+                        Controller.getController().controller.get(keyTeamEliminate).listDoctors.remove(indexEliminated);
+                        System.out.println("\n* * * * * * * * * * * * * * * * *");
+                        System.out.println("Doctor eliminado exitosamente.\n* * * * * * * * * * * * * * * * *\n\n>>> Presiona una tecla para volver al menú.");
+                    }
+                }
+                else {
+                    System.out.println("\n:: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: ::");
+                    System.out.println("::  No se ha creado ningún equipo con ese código.  ::\n:: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: ::");
+                    System.out.println("\n>>> Presiona una tecla para volver al menú.");
+                }
+
                 DoctorMenu();
                 break;
 
